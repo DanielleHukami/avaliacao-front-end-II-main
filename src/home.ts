d90c48695459d3a0f5a4b89a76c92c95da7d7612
Danielle Cristine Hukami <dani.hukami@gmail.com>
2022-06-20 02:14:32 -0300

    storage

--- src/home.ts
+++ src/home.ts
@@ -1,10 +1,52 @@
-let modalCadastro = new bootstrap.Modal('#exampleModal')
+/* INPUTS */
 let inputDescricao = document.getElementById('input-descricao') as HTMLInputElement
-let inputDatalhamento = document.getElementById('input-datalhamento') as HTMLTextAreaElement
-let btnSlavar = document.getElementById('btn-salvar') as HTMLButtonElement
+let inputDetalhamento = document.getElementById('input-detalhamento') as HTMLInputElement
+let inputEditarDescricao = document.getElementById('input-editar-descricao') as HTMLInputElement
+let inputEditarDetalhamento = document.getElementById('input-editar-detalhamento') as HTMLInputElement
 
-btnSlavar.addEventListener('click', () => {
-    console.log(inputDescricao.value)
-    console.log(inputDatalhamento.value)
-    modalCadastro.hide()
+/* BOTÕES */
+let btnSalvarRecado = document.getElementById('btn-salvar-recado') as HTMLButtonElement
+let btnEditarRecado = document.getElementById('btn-atualizar') as HTMLButtonElement
+let btnApagarRecado = document.getElementById('btn-apagar') as HTMLButtonElement
+
+/* MODAIS */
+let modalEditarRecado = new bootstrap.Modal('#modalEditar')
+let modalApagarRecado = new bootstrap.Modal('#modalApagar')
+
+
+/* EVENTOS */
+btnSalvarRecado.addEventListener('click', (e) => {
+    e.preventDefault()
+    salvarRecado()
 })
+
+/* INTERFACE */
+interface Recado {
+    codigo: string,
+    descricao: string,
+    detalhamento: string
+}
+
+/* FUNÇÕES */
+function salvarRecado(){
+
+    let listaRecados: Recado[] = []
+
+    let novoRecado: Recado = {
+        codigo: '1',
+        descricao: inputDescricao.value,
+        detalhamento: inputDetalhamento.value
+    }
+
+    listaRecados.push(novoRecado)
+    salvarNoStorage(listaRecados)
+    console.log(listaRecados)
+
+}
+
+function salvarNoStorage(recados: Recado[] = []){
+
+   let listaUsuarios: Array<Usuario> = JSON.parse(localStorage.getItem('usuarios') || '[]')
+   let checagem
+
+}
