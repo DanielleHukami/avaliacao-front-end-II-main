1cf8464dd8adada6e851a0f1734732ac7a4c0c2d
Danielle Cristine Hukami <dani.hukami@gmail.com>
2022-06-19 22:51:39 -0300

    página de recados - modelo passado em revisão - testes

--- public/dist/home.js
+++ public/dist/home.js
@@ -1,15 +1,10 @@
 "use strict";
-document.addEventListener('DOMContentLoaded', () => {
-    let usuarioLogado = sessionStorage.getItem('usuarioLogado');
-    if (!usuarioLogado) {
-        window.alert("Necessário realizar o login");
-        window.location.href = 'http://127.0.0.1:5500/index.html';
-    }
-    let listaUsuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
-    let dadosUsuarioLogado = listaUsuarios.find((usuario) => {
-        return usuario.email === usuarioLogado;
-    });
-    let indiceLogado = listaUsuarios.findIndex((usuario) => {
-        return usuario.email === usuarioLogado;
-    });
+let modalCadastro = new bootstrap.Modal('#exampleModal');
+let inputDescricao = document.getElementById('input-descricao');
+let inputDatalhamento = document.getElementById('input-datalhamento');
+let btnSlavar = document.getElementById('btn-salvar');
+btnSlavar.addEventListener('click', () => {
+    console.log(inputDescricao.value);
+    console.log(inputDatalhamento.value);
+    modalCadastro.hide();
 });
--- src/home.ts
+++ src/home.ts
@@ -1,21 +1,10 @@
+let modalCadastro = new bootstrap.Modal('#exampleModal')
+let inputDescricao = document.getElementById('input-descricao') as HTMLInputElement
+let inputDatalhamento = document.getElementById('input-datalhamento') as HTMLTextAreaElement
+let btnSlavar = document.getElementById('btn-salvar') as HTMLButtonElement
 
-document.addEventListener('DOMContentLoaded', () => {
-
-    let usuarioLogado = sessionStorage.getItem('usuarioLogado')
-
-    if(!usuarioLogado){
-        window.alert("Necessário realizar o login")
-        window.location.href = 'http://127.0.0.1:5500/index.html' 
-    } 
-
-    let listaUsuarios =  JSON.parse(localStorage.getItem('usuarios') || '[]')
-    let dadosUsuarioLogado = listaUsuarios.find((usuario: { email: string | null }) =>{
-        return usuario.email === usuarioLogado 
-    })
-
-    let indiceLogado = listaUsuarios.findIndex((usuario: { email: string | null }) => {
-        return usuario.email === usuarioLogado
-    })
-    
+btnSlavar.addEventListener('click', () => {
+    console.log(inputDescricao.value)
+    console.log(inputDatalhamento.value)
+    modalCadastro.hide()
 })
- 
